6e0b26c8170c76f8ab43c8e58fa81b488db3d16c
dsandrade <danielsanto.andrade@hotmail.com>
2018-05-30 02:04:57 +0000

    Simple vue skeleton added to project

---
+++ resources/assets/js/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <Example/>
+</template>
+
+<script>
+    import Example from '../components/ExampleComponent'
+
+    export default {
+        name: 'home',
+        components: {
+            Example
+        }
+    }
+</script>
