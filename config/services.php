af6fad3c358c5979ec4b290ea5b4a678e4df17f8
dsandrade <danielsanto.andrade@hotmail.com>
2018-05-31 02:07:43 +0000

    Fix model path

--- config/services.php
+++ config/services.php
@@ -30,7 +30,7 @@ return [
     ],
 
     'stripe' => [
-        'model' => App\User::class,
+        'model' => Dream\Boilerplate\Models\User::class,
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
